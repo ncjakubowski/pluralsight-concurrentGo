7568ab1288042698d5e3fa5f863b58068ce921c6
Nicholas Jakubowski <nicholas.jakubowski@accenture.com>
2018-02-06 16:04:17 -0800

    Initial working example of a go routine

---
+++ ex1.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"time"
+)
+
+func main() {
+	go func() {
+		println("Hello")
+	}()
+
+	go func() {
+		println("Go")
+	}()
+
+	dur, _ := time.ParseDuration("1s")
+	time.Sleep(dur)
+}
